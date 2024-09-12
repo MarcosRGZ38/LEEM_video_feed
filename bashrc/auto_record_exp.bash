edeefffd2635b7b7a810b0c8f68acf14988106e7
Marcos Rodríguez Delgado <marcos.rdelgado@alumnos.upm.es>
2024-09-12 10:04:35 +0200

    idk what im doin

--- bashrc/auto_record_exp.bash
+++ bashrc/auto_record_exp.bash
@@ -14,11 +14,12 @@ CAM_LOGS="/home/leem/video-exp/cam-logs.txt"
 
 
 # Available options (sudo libcamera-vid --list-cameras)
-# OV5647: 640x480 58, 1296x972 43, 1920x1080 30, 2592x1944 15
-# IMX219: 640x480 30, 1640x1232 30, 1920x1080 30, 3280x2464 30
+# OV5647(V1) : 640x480 58, 1296x972 43, 1920x1080 30, 2592x1944 15
+# IMX219(V2) : 640x480 30, 1640x1232 30, 1920x1080 30, 3280x2464 30
 
 # Under test:
 # sudo libcamera-vid --width 1640 --height 1232 --framerate 60 -o output.raw
+
 
 
 set +e
@@ -29,7 +30,7 @@ trap "echo 'Script terminated at $(date)' >> $OUT_LOGS; exit" SIGINT SIGTERM
 
 while true
 do
-  OUT_FILE_MP4="${SAVE_DIR}$(date +"exp-vid-%Y-%m-%dT%H-%M-%S.mp4")"
+  OUT_FILE_MP4="${SAVE_DIR}$(date +"exp-vid-%Y-%m-%dT%H-%M-%S.raw")"
   timeout $((VIDEOT_S+10))s libcamera-vid --codec libav --width $CAM_WIDTH --height $CAM_HEIGHT --framerate $CAM_FPS -->
   if [[ $? -ne 0 ]]; then
     echo "$(date): Error with libcamera-vid: See $CAM_LOGS for details" >> $OUT_LOGS
@@ -37,7 +38,7 @@ do
 
   sudo find "$SAVE_DIR" -type f -mmin +$AUTO_REMOVE_MIN -print -delete | while read file; do
     echo deleted file >> /dev/null
-    #echo "Deleted file: $file" >> $OUT_LOGS
+    echo "Deleted file: $file" >> $OUT_LOGS
   done
 
   sleep 2
